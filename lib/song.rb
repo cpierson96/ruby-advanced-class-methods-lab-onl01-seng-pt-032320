97e4503e50e8800cb74e40bcbfaec49b0b8b1701
Claudia Pierson <c172@umbc.edu>
2020-04-23 03:38:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,9 +37,8 @@ class Song
     @@all.find{|song| song.name == name}
   end
 
-  def self.find_or_create_by_name
-    self.create_by_name
-    self.find_by_create_by_name
+  def self.find_or_create_by_name(song)
+
   end
 
   def self.new_from_filename
@@ -49,7 +48,7 @@ class Song
   end
 
   def self.destroy_all
-
+    self.all.clear
   end
 
 
