5476ffa7010dd6e4fc30f380773b87ce99156184
Claudia Pierson <c172@umbc.edu>
2020-04-23 03:49:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,7 +41,7 @@ class Song
     self.find_by_name(song) || self.create_by_name(song)
   end
 
-  def self.alphabetical(name)
+  def self.alphabetical
     self.all.sort_by{|song| song.name}
   end
 
