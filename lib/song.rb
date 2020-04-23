baac904a7d6619312301b4dac8c6bce14afbe215
Claudia Pierson <c172@umbc.edu>
2020-04-23 04:16:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -58,7 +58,7 @@ class Song
 
   def self.create_from_filename(filename)
     result = self.new_from_filename(filename)
-    song = self.create
+    song = self.save
     song.name = result.name
     song.artist_name = result.artist_name
     song
