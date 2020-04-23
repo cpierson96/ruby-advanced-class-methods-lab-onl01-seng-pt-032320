2c4e0039a5c9987df8be110c397b57741fdded06
Claudia Pierson <c172@umbc.edu>
2020-04-23 02:46:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,11 +15,12 @@ class Song
   def save
     self.class.all << self
   end
-
+  # .create
+  #    instantiates and saves the song, and it returns the new song that was created
   def self.create
     song = Song.new
     song.save
-     song 
+    return song 
   end
 
   def self.new_by_name
