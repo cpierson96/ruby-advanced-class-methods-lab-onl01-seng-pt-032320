a98981c2f94d0962b9cb6b246ed42f4d3d1487a2
Claudia Pierson <c172@umbc.edu>
2020-04-23 03:54:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,9 +2,8 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
-  def initialize(name, artist_name)
-    @name = name
-    @artist_name = artist_name
+  def initialize
+    
 
   end
 
