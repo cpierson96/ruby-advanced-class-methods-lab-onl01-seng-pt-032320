20dda782518d3f8e7dbce54d645af7cf82ade644
Claudia Pierson <c172@umbc.edu>
2020-04-23 03:12:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,10 +33,12 @@ class Song
     song = Song.create
     song.name = name
     song 
-
   end
 
-  def self.find_by_name
+  def self.find_by_name(name)
+    song = Song.create 
+    song.name = name 
+    song.find 
   end
 
   def self.find_or_create_by_name
