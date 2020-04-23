27f27654d99af8c35c39ca2e9e05fb55ffa14eea
Claudia Pierson <c172@umbc.edu>
2020-04-23 02:38:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,12 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def initialize 
+    @name = name 
+    @artist_name = artist_name 
+  
+  end 
+  
   def self.all
     @@all
   end
@@ -9,5 +15,31 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create 
+    @@all.self 
+  end 
+  
+  def self.new_by_name 
+  end 
+   
+  def self.create_by_name 
+  end 
+  
+  def self.find_by_name 
+  end 
 
+  def self.find_or_create_by_name
+  end 
+
+  def self.new_from_filename
+  end 
+
+  def self.create_from_filename
+  end 
+
+  def self.destroy_all 
+  end 
+  
+  
 end
